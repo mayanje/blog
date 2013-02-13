8e8e96bc17f24d29fa9e6e0bdc6982bfc80a58cb
JONATHAN PARK <jonathanpark@silve-mac.(none)>
2013-02-12 22:29:32 -0800

    Adding foreman procfile and server setup

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var static = require('node-static');
 // Create a node-static server to serve the current directory
 //
 var file = new(static.Server)('./public', { cache: 0});
-
+var port = process.env.PORT ? process.env.PORT : 8080
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
         //
@@ -20,6 +20,6 @@ require('http').createServer(function (request, response) {
             }
         });
     });
-}).listen(8080);
+}).listen(port);
 
-console.log("> node-static is listening on http://127.0.0.1:8080");
+console.log("> node-static is listening on http://127.0.0.1:"+port);
